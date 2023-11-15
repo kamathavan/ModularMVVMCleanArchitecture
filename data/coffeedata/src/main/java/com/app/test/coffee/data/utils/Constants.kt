5f2477e8b53ab8883d7cb38fcc461ace9227080b
Mathavan Kaliyaperumal <mathucs@gmail.com>
2023-11-15 19:21:43 +0530

    code cleaned

--- data/coffeedata/src/main/java/com/app/test/coffee/data/utils/Constants.kt
+++ data/coffeedata/src/main/java/com/app/test/coffee/data/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.app.test.coffee.data.utils
 object Constants {
     const val BASE_URL = "https://api.sampleapis.com/"
     const val COFFEES_ENDPOINT = "coffee/hot"
-    const val TIME_OUT = 30
+    const val TIME_OUT = 100
     const val SEPARATOR  = ","
 }
