084a6913db1d21636401c32445a9b6ceb672e6ce
Mathavan Kaliyaperumal <mathucs@gmail.com>
2023-12-12 13:22:17 +0530

    feature:commit 1

--- features/coffee/src/main/java/com/app/feature/coffee/ui/coffeelist/CoffeeHomeScreen.kt
+++ features/coffee/src/main/java/com/app/feature/coffee/ui/coffeelist/CoffeeHomeScreen.kt
@@ -41,6 +41,9 @@ fun CoffeeHomeScreen(viewModel: CoffeeListViewModel = hiltViewModel()) {
             is CoffeeUiState.Error -> {
                 ErrorScreen(it.message)
             }
+
+            // commit1
+            // commit2
         }
     }
 
